ebd26baeb65d2e135b2d9b6353b3b8b91c43fab7
KINOTheProducer <kinotheproducer@gmail.com>
2023-03-09 13:55:47 -0500

    start of building out match info array

--- pages/api/matchinfo.js
+++ pages/api/matchinfo.js
@@ -1,5 +1,5 @@
-// running the user's query through Riot's API - this will return the summoner's info
-// we will extract the puuid identifier from the summoner info endpoint
+// initially, we will be running the user's query through Riot's summoners endpoint - this will return the summoner's info
+// we will extract the puuid identifier from that information
 // we will plug that into the matches endpoint to return the match IDs for the last 5 games they've played
 
 export default async function handler(req, res) {
@@ -10,6 +10,23 @@ export default async function handler(req, res) {
     console.log(data)
     const puuid = data.puuid;
     const matchListResponse = await fetch(`https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=5&api_key=${api}`);
-    const matchList = await matchListResponse.json();
-    console.log(matchList);
+    const matchListData = await matchListResponse.json();
+    console.log(matchListData);
+
+    // setting a blank array to store the match information that we will grab and send to our frontend
+    const matches = [];
+
+    // creating a loop that will go through each match individually and return more detailed information
+    // we will this information to find the info we need for the queried match, and only return what we want to show on the frontend
+    for (let i = 0; i < matchListData.length; i++) {
+        const response = await fetch(`https://americas.api.riotgames.com/lol/match/v5/matches/${matchListData[i]}?api_key=${api}`);
+        const data = await response.json();
+
+        // grabbing the raw duration of each match in milliseconds and converting that to a string of minutes and seconds
+        const durationRaw = data.info.gameDuration;
+        const durationMin = Math.floor(durationRaw / 60);
+        const durationSec = durationRaw % 60;
+        const durationString = `${durationMin}m ${durationSec}s`;
+        console.log(durationString)
+    }
 }
